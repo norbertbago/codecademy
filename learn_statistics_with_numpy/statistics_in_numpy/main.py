07831d8403a451ee4b3ad86def86250169dd955d
norbert.bago <norbert.bago@subject.sk>
2021-03-05 21:22:07 +0100

    statistic in numpy

---
+++ learn_statistics_with_numpy/statistics_in_numpy/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+store_one = np.array([2, 5, 8, 3, 4, 10, 15, 5])
+
+#average of store_one
+mean_store  = np.mean(store_one)
+
+#percentage of number more or equeal than 2
+mean_store1 = np.mean(store_one >= 2)
